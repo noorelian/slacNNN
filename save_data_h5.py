dc3abaefd945a88e9a53243f442c2e7c1c10ba24
Neveu <nneveu@stanford.edu>
2026-05-05 14:25:50 -0700

    trying to speed up loop

--- save_data_h5.py
+++ save_data_h5.py
@@ -27,12 +27,11 @@ def save_filenames_to_txt(quench_files, output_txt):
 
 def load_quench_data(quench_files):
     """Load quench files and return DataFrame of all quench waveforms."""
-    quench_data = pd.DataFrame() 
+    quench_data = []
     for filename in quench_files:
         df = load_fault_file(filename)
-        waveforms   = grab_common_data(df)
-        quench_data = pd.concat([quench_data, waveforms], ignore_index=True)
-    return quench_data
+        quench_data.append(grab_common_data(df))
+    return pd.concat(quench_data, ignore_index=True)
 
 # def _return_all_cm_data(all_data, cm):
 #     """Return all data for a given cryomodule."""
@@ -40,13 +39,15 @@ def load_quench_data(quench_files):
 
  
 # --- Main execution block ---
-all_data = pd.DataFrame()
+all_data = [] #pd.DataFrame()
 for lx in range(4): 
     # For each Lx section
     quench_files = _get_quench_filenames(lx)
     #output_txt = f"quench_files_L{lx}.txt"
     #save_filenames_to_txt(quench_files, output_txt)
-    all_data  = pd.concat([all_data, load_quench_data(quench_files)], ignore_index=True)
+    all_data.append(load_quench_data(quench_files))
+all_data  = pd.concat(all_data, ignore_index=True)
+all_data.to_pickle(f"all_quench_data.pkl")
 
 with h5py.File(f"quench_data_L0-L3.h5", 'w') as h5file:
     for (cm, cav), cav_data in all_data.groupby(["cryomodule", "cavity"], dropna=False):
@@ -104,24 +105,6 @@ with h5py.File(f"quench_data_L0-L3.h5", 'w') as h5file:
 #         increment_quench_count(day_group)
 
 #         quench_group = day_group.create_group(timestamp)
-
-#         # constructing PV label strings
-#         cavity_faultname = pv_base + ':CAV:FLTAWF'
-#         forward_pow = pv_base + ':FWD:FLTAWF'
-#         reverse_pow = pv_base + ':REV:FLTAWF'
-#         decay_ref = pv_base + ':DECAYREFWF'  
-#         time_range = pv_base + ':CAV:FLTTWF'
-#         q_value = pv_base + ":QLOADED"          
-#         freq_value = pv_base + ":FREQ" 
-
-#         # extracting all data for quench waveform using defined function
-#         cavity_data, cavity_time = extracting_data(file, cavity_faultname)
-#         forward_data, forward_time = extracting_data(file, forward_pow)
-#         reverse_data, reverse_time = extracting_data(file, reverse_pow)
-#         decay_data, decay_time = extracting_data(file, decay_ref)
-#         time_data, time_timestamp = extracting_data(file, time_range)
-#         q_data, q_time = extracting_data(file, q_value)
-
 #         # using try-except statement to catch where fault_data[0] is 'None'
 #         # idea: file may be corrupt or waveform may be weird
 #         try:
@@ -131,26 +114,3 @@ with h5py.File(f"quench_data_L0-L3.h5", 'w') as h5file:
 #             quench_group.attrs['calculated_q_value'] = calculated_q
 #         except IndexError as e:
 #             print(f"Processing {filename} failed with {e}")
-
-#         # making them all the same length in case the length varies
-#         forward_data = forward_data[:len(cavity_data)]
-
-#         # saving waveform data into the group
-#         quench_group.create_dataset('time_seconds', data=time_data)
-#         quench_group.create_dataset('cavity_amplitude_MV', data=cavity_data)
-#         quench_group.create_dataset('forward_power_W2', data=forward_data)
-#         quench_group.create_dataset('reverse_power_W2', data=reverse_data)
-        
-#         if decay_data is not None:
-#             quench_group.create_dataset('decay_reference_MV', data=decay_data)
-#         else:
-#             print(f"Warning: decay_data is None for {filename}")
-
-#         # saving metadata for each quench as attributes
-#         quench_group.attrs['filename'] = f"{filename}"
-#         quench_group.attrs['timestamp'] = cavity_time
-#         quench_group.attrs['faultname'] = cavity_faultname
-#         quench_group.attrs['cavity_number'] = parts[2][2]
-#         quench_group.attrs['cryomodule'] = parts[2][:2] 
-
-# print(f"Data from {len(quench_files)} files successfully saved to {output_filename}.")

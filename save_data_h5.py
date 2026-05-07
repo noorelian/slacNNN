83d864e6a2ef50ce462ad339e44ad11677b02324
Neveu <nneveu@stanford.edu>
2026-05-07 14:03:56 -0700

    fixes for H1/H2

--- save_data_h5.py
+++ save_data_h5.py
@@ -13,13 +13,14 @@ else:
     DATA_DIR = r"/mccfs2/u1/lcls/physics/rf_lcls2/fault_data/"
     savefile = "/sdf/group/ad/org/lfd/sclp/data/quench_data_L0-L3.h5"
 
-def _get_lx_dir(area):
-    """Get accelerating area directory (e.g. 'L0B', 'L1B', 'H1B', 'H2B')."""
-    return os.path.join(DATA_DIR, f"ACCL_{area}_*")
+def _get_lx_dir(lx): 
+    """Get accelerating section, L0, L1, L2, or L3 directory."""
+    lnum = f"{lx:1d}"
+    return os.path.join(DATA_DIR, f"ACCL_L{lnum}B_*")
 
-def _get_quench_filenames(area):
-    """Get sorted quench files for an area."""
-    lx_dir = _get_lx_dir(area)
+def _get_quench_filenames(lx):
+    """Get sorted quench files for Lx section."""
+    lx_dir = _get_lx_dir(lx)
     quench_files = glob.glob(os.path.join(lx_dir, '**', '*QUENCH.txt'), recursive=True)
     return sorted(quench_files)
 
@@ -39,10 +40,10 @@ def save_filenames_to_txt(quench_files, output_txt):
  
 # --- Main execution block ---
 all_data = [] 
-for area in ["L0B", "L1B", "L2B", "L3B", "H1B", "H2B"]:
-    quench_files = _get_quench_filenames(area)
+for lx in range(4): 
+    quench_files = _get_quench_filenames(lx)
     #save_filenames_to_txt(quench_files, output_txt)
-    print(f"{area}: found {len(quench_files)} quench files")
+    print(f"L{lx}: found {len(quench_files)} quench files")
     all_data.append(load_quench_data(quench_files))
 all_data  = pd.concat(all_data, ignore_index=True)
 #all_data.to_pickle(f"all_quench_data.pkl")
@@ -51,8 +52,8 @@ with h5py.File(savefile, 'w') as h5file:
     for (cm, cav), cav_data in all_data.groupby(["cryomodule", "cavity"], dropna=False):
         
         print(f"Processing CM{cm} CAV{cav}...")
-        cm_group     = h5file.require_group(f"CM{int(cm)}")
-        cav_group    = cm_group.require_group(f"CAV{int(cav)}")
+        cm_group     = h5file.require_group(f"CM{cm}")
+        cav_group    = cm_group.require_group(f"CAV{cav}")
         quench_files = cav_data['source_file'].unique()
         
         print(f"Data for CM{cm} CAV{cav} has {len(quench_files)} quench events.")

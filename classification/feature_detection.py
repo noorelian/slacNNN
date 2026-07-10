4b6a60c6b2befc76f4648095f4022b22297f72ba
norahao <norah@ambytechnologies.com>
2026-07-10 12:30:41 -0700

    Folder and file organization for classification system

---
+++ classification/feature_detection.py
@@ -0,0 +1,7 @@
+"""
+if average_pre_quench_amplitude < 10 MV: return "Cavity Off"
+
+average_pre_puench_amplitude();
+    from t = -0.02 to t = -0.01
+        calculate average
+"""
--- classify.py
+++ classify.py
@@ -1,15 +1,34 @@
+from utils.save_data_h5 import load_quench_data
+
 """
-from analyzer.fitting import
-from analyzer.spike_detection import
-from analyzer.amplitude import
-
-main
+check for average? straighntness? noise level?
+"""
 
 
+def min_pre_quench_amplitude():
+    pass
 
+
+def area():
+    pass
+
+
+def exponential_decay_fit():
+    pass
+
+
+def power_spike_detection():
+    pass
+
+
+def main():
+    data = load_quench_data()
+
+    min_pre_quench_amplitude()
+    area()
+    exponential_decay_fit()
+    power_spike_detection()
 
 
 if __name__ == "__main__":
     main()
-
-"""

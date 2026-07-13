27315943ba04e47e6466f3e51c9369a55502803e
norahao <norah@ambytechnologies.com>
2026-07-13 14:03:56 -0700

    Added data loading for classification

--- classify.py
+++ classify.py
@@ -1,11 +1,25 @@
-from utils.save_data_h5 import load_quench_data
+import h5py  # type: ignore
 
-"""
-check for average? straighntness? noise level?
-"""
+import numpy as np
+from dataclasses import dataclass, fields
+from typing import Optional
+from numpy.typing import NDArray
 
 
-def min_pre_quench_amplitude():
+@dataclass
+class QuenchEvent:
+    fault_time: NDArray[np.float64]
+    fault_waveform: NDArray[np.float64]
+    forward_power: NDArray[np.float64]
+    forward_time: NDArray[np.float64]
+    reverse_power: NDArray[np.float64]
+    reverse_time: NDArray[np.float64]
+
+    # Optional array, also strictly typed
+    decay_reference: Optional[NDArray[np.float64]] = None
+
+
+def pre_quench_amplitude(quench_event_data: QuenchEvent):
     pass
 
 
@@ -21,13 +35,31 @@ def power_spike_detection():
     pass
 
 
-def main():
-    data = load_quench_data()
+def load_quench_event(file_path: str, event_path: str) -> QuenchEvent:
+    with h5py.File(file_path, "r") as f:
+        event_group = f[event_path]
+        if not isinstance(event_group, h5py.Group):
+            raise TypeError(
+                f"Expected an h5py.Group at {event_path}, got {type(event_group)}"
+            )
+        data_dict = {}
 
-    min_pre_quench_amplitude()
-    area()
-    exponential_decay_fit()
-    power_spike_detection()
+        for field in fields(QuenchEvent):
+            if field.name in event_group:
+                item = event_group[field.name]
+                if isinstance(item, h5py.Dataset):
+                    data_dict[field.name] = item[:]
+
+        return QuenchEvent(**data_dict)
+
+
+def main():
+    file_path = "/Users/norah.ao/Documents/SLACPython/data/quench_data_L0.h5"
+    event_path = "CM01/CAV1/20220329_103006"
+
+    quench_event_data: QuenchEvent = load_quench_event(file_path, event_path)
+
+    pre_quench_amplitude(quench_event_data)
 
 
 if __name__ == "__main__":

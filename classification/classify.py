3de6b660e06d9096688e2dab82eb1405fb44676d
norahao <norah@ambytechnologies.com>
2026-07-23 19:41:36 -0700

    Deleted models.py and moved code to classify.py

--- classification/classify.py
+++ classification/classify.py
@@ -3,9 +3,32 @@ from pathlib import Path
 from typing import Iterator, Tuple, Dict, Any
 
 from utils.config import DATA_DIR
-from .models import QuenchData
 from .data_loader import load_specific_quench_events
 from .logic import classify
+
+from enum import Enum
+from dataclasses import dataclass
+import numpy as np
+from numpy.typing import NDArray
+from typing import Optional
+
+
+@dataclass
+class QuenchData:
+    fault_time: NDArray[np.float64]
+    fault_waveform: NDArray[np.float64]
+    forward_power: NDArray[np.float64]
+    forward_time: NDArray[np.float64]
+    reverse_power: NDArray[np.float64]
+    reverse_time: NDArray[np.float64]
+    decay_reference: Optional[NDArray[np.float64]] = None
+
+
+class QuenchStatus(Enum):
+    real = "real"
+    false = "false"
+    other = "other"
+    cavity_off = "cavity_off"
 
 
 def run_classification(

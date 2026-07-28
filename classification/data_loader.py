811e2d58d1c80dd8436cc4061a73c0198e2c11fd
nelian <nelian1@student.ohlone.edu>
2026-07-28 09:44:36 -0700

    updated some comments

--- classification/data_loader.py
+++ classification/data_loader.py
@@ -8,10 +8,6 @@ from numpy.typing import NDArray
 from utils.config import DATA_DIR
 
 
-"""
-def load_all_quench_events() -> Iterator[Tuple[str, QuenchData]]:
-    folder = Path(DATA_DIR)
-
 @dataclass
 class QuenchData:
     fault_time: NDArray[np.float64]
@@ -58,35 +54,3 @@ def load_quench_events(file_pattern: str = "*.h5") -> Iterator[Tuple[str, Quench
                                     data_dict[field.name] = item[:]
 
                         yield (event_id, QuenchData(**data_dict))
-"""
-
-
-def load_specific_quench_events() -> Iterator[Tuple[str, QuenchData]]:
-    folder = Path(DATA_DIR)
-
-    for h5_file in folder.glob("*L0.h5"):
-        with h5py.File(h5_file, "r") as f:
-            cm_name = "CM01"
-            cav_name = "CAV1"
-
-            if cm_name not in f or cav_name not in f[cm_name]:  # type: ignore
-                continue
-
-            cav_group = f[cm_name][cav_name]  # type: ignore
-
-            for timestamp, event_group in cav_group.items():  # type: ignore
-                if not isinstance(event_group, h5py.Group):
-                    continue
-
-                event_id = f"{cm_name}/{cav_name}/{timestamp}"
-
-                data_dict = {}
-                for field in fields(QuenchData):
-                    if field.name in event_group:
-                        item = event_group[field.name]
-                        if isinstance(item, h5py.Dataset):
-                            data_dict[field.name] = item[:]
-
-                yield (event_id, QuenchData(**data_dict))
-
-

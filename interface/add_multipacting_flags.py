3a29768301029c6bac6ab9061a9d6051fd94727e
nelian <nelian1@student.ohlone.edu>
2026-07-28 09:35:40 -0700

    added some comments

--- interface/add_multipacting_flags.py
+++ interface/add_multipacting_flags.py
@@ -3,29 +3,40 @@ import pandas as pd
 import numpy as np
 
 def add_multipacting_flags(file_path, multipacting_file, flag_attr='Multipacting'):
-    dataframe = load_csv(multipacting_file)
+    """
+    Add a multipacting flag to each events in the h5 file
+    - The function matches the event name in the h5 file with the data in the csv file 
+    - Each event in the h5 file gets a boolean attribute so either true or false 
+    - Match only after ignoring the time (HHMMSS) in the h5 file since the events are saved with no time records
+    """
+    dataframe = load_csv(multipacting_file) # Load the csv file into a dataframe 
 
+    # build a set of keys to identify each multipacting event
     multipacting_keys = set()
     for _, row in dataframe.iterrows():
         key =(
-            str(row['cm']).strip(),
-            str(row['cav']).strip(),
-            f"{int(row['year']):04d}",
-            f"{int(row['month']):02d}",
-            f"{int(row['day']):02d}",
+            str(row['cm']).strip(),     # cryomodule as a string
+            str(row['cav']).strip(),    # cavity as a string 
+            f"{int(row['year']):04d}",  # year : 4 digits, e.g. 2025
+            f"{int(row['month']):02d}", # month : 2 didgits, e.g. 07
+            f"{int(row['day']):02d}",   # day : 2 digits, e.g. 19
         )
         multipacting_keys.add(key)
 
-    matched = 0
-    total = 0
+    matched = 0     # flagged events 
+    total = 0       # valid processed events 
 
+    # read the h5 file 
     with h5py.File(file_path, 'a') as f :
         event_paths = []
         f.visit(lambda name: event_paths.append(name))
 
+        # Loop over each path 
         for path in event_paths:
-            parts = path.split('/')
+            parts = path.split('/')     # split like this ['CM01', 'CAV1', '20240116_103022']
 
+            # A real event has exactly 3 parts: CM / CAV / timestamp
+            # Skip anything else (like top-level CM or CAV groups)
             if len(parts) != 3:
                 continue
 
@@ -49,6 +60,13 @@ def add_multipacting_flags(file_path, multipacting_file, flag_attr='Multipacting
                 matched += 1
 
 def load_csv(path):
+    """
+    load the csv or txt files into pandas dataframe 
+
+    - csv files are read direclty 
+    - txt files are comma-seperated first, if that fails it fall back to whitespace-seperated 
+    - Otherwise, it throws an error 
+    """
     if path.endswith('.csv'):
         return pd.read_csv(path)
     elif path.endswith('.txt'):
